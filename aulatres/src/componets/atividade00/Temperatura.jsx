679694cf752e9a006684b5d3e1ea6f09b0ba5ece
Samuel Nascimento <101434230+samuelc-dev@users.noreply.github.com>
2023-03-26 13:46:39 -0300

    Update Temperatura.jsx

--- aulatres/src/componets/atividade00/Temperatura.jsx
+++ aulatres/src/componets/atividade00/Temperatura.jsx
@@ -9,19 +9,22 @@ const Temperatura = () => {
 
     function kelvin(kelvin) {
         return (
-            {
-                "fahrenheit":1.8 * (kelvin - 273) + 32,
-                "celsius":kelvin - 273
-            }
+            const fahrenheit = 1.8 * (kelvin - 273) + 32;
+            const celsius = kelvin - 273;
+            return {celsius, fahrenheit};
         );
     }
-
+    
+    const celsius = 25;
+    const fahrenheit = 25
+    const kelvin = 0;
+    const {celsius, fahrenheit} = kelvin(0);
     return (
         <div>
-            <h1>25 celsius para Fahrenheit é: {celsiusParaFahrenheit(25).toFixed(2)}</h1>
-            <h1>25 Fahrenheit para celsius é: {fahrenheitParaCelsius(25).toFixed(2)}</h1>
-            <h1>25 kelvin para celsius é: {kelvin(25).celsius.toFixed(2)}</h1>
-            <h1>25 kelvin para fahrenheit é: {kelvin(25).fahrenheit.toFixed(2)}</h1>
+            <h1>25 celsius para Fahrenheit é: {celsiusParaFahrenheit(celsius).toFixed(2)}</h1>
+            <h1>25 Fahrenheit para celsius é: {fahrenheitParaCelsius(fahrenheit).toFixed(2)}</h1>
+            <h1>De kelvin para celsius é: {celsius.toFixed(2)}</h1>
+            <h1>De kelvin para fahrenheit é: {fahrenheit.toFixed(2)}</h1>
         </div>
     )
 }
